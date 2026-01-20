24a304a208fdbd91704cbc976260965984712b6d
SousdeyKim <sousdey716@gmail.com>
2026-01-20 19:01:39 +0700

    Change to BrowserRouth

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(() => ({
-  base: "/Portfolio_Sousdey_Kim/",
+export default defineConfig(({ mode }) => ({
+  // Use "/" in dev, repo name in production build
+  base: mode === "production" ? "/Portfolio_Sousdey_Kim/" : "/",
+
   server: {
     host: "::",
     port: 8080,
-    hmr: {
-      overlay: false,
-    },
+    hmr: { overlay: false },
   },
-  plugins: [react()], // Lovable removed
+
+  plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: { "@": path.resolve(__dirname, "./src") },
   },
 }));
